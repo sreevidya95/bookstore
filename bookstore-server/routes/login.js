c2c9b28828fa7922b7e8b8789cde97ed83d19f80
sreevidya <sreevidya-bathalapa@pluralsight.com>
2024-07-01 20:51:00 +0530

    implemented login page routes

--- bookstore-server/routes/login.js
+++ bookstore-server/routes/login.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const { Sequelize } = require('sequelize');
 const mail = require('nodemailer');
-const sequelize = new Sequelize("bs", 'root', 'Saibaba123456@', {
-    host: "localhost",
-    dialect: "mysql",
-});
+const {sequelize} = require("../models/sequelize");
 const Admin = require("../models/Admin")(sequelize);
+async function authenticateAdmin(email){
+    const admin = await Admin.findOne({
+        where:{
+            email:email,
+        }
+    });
+    return admin; 
+}
 router.post('/',async (req,res)=>{
     try{
-        const admin = await Admin.findOne({
-            where:{
-                email:req.body.email,
-            }
-        });
+        const admin = await authenticateAdmin(req.body.email);
            if(admin){
-            console.log(admin.get("password"))
             const valid = await bcrypt.compare(req.body.password, admin.get("password"));
             if(valid){
                 res.status(200).json(admin);
+            }
+            else{
+                res.status(404).json({error:"invalid password"})
             }    
            }
            else{
@@ -33,28 +35,37 @@ router.post('/',async (req,res)=>{
     
 });
 router.post('/fp', async(req,res)=>{
-   const transport = mail.createTransport({
-    service:'gmail',
-    auth:{
-        user:'tapp93550@gmail.com',
-        pass:'hijl zvbh ciyg sacu'
+    try{
+        const admin = authenticateAdmin(req.body.email);
+        if(admin){
+            const transport = mail.createTransport({
+                service:'gmail',
+                auth:{
+                    user:'tapp93550@gmail.com',
+                    pass:'hijl zvbh ciyg sacu'
+                }
+            });
+            var mailOptions={
+                from:'tapp93550@gmail.com',
+                to:req.body.email,
+                subject:'forgot passsword',
+                text:`here is your link to create new password for book store application`
+            };
+            transport.sendMail(mailOptions,(err,response)=>{
+                if(err){
+                res.status(500).json({error:err});
+                }
+                else{
+                res.status(200).json({msg:"Email sent"});
+                }
+            });
+        }
+        else{
+            res.status(404).json({error:"Seems like you dont have the account"});
+        }
+    }catch(e){
+        res.status(500).json({error:e.message});
     }
-});
-var mailOptions={
-    from:'tapp93550@gmail.com',
-     to:req.body.email,
-     subject:'forgot passsword',
-     text:`here is your link to create new password for book store application`
-};
-transport.sendMail(mailOptions,(err,response)=>{
-    if(err){
-       res.status(500).json({error:err});
-    }
-    else{
-       res.status(200).json({msg:"Email sent"});
-    }
-});
-
 });
 router.post("/new",async(req,res)=>{
       try{
