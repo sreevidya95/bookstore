3b98111f0504af8bc34d8fefedc179c8811ca642
sreevidya <sreevidya-bathalapa@pluralsight.com>
2024-07-01 12:47:11 +0530

    implemented CRUD operations fro authors table

--- bookstore-server/routes/authors.js
+++ bookstore-server/routes/authors.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {Sequelize} = require('sequelize');
+const {Sequelize, where} = require('sequelize');
 const sequelize = new Sequelize("bs",'root','Saibaba123456@',{
     host:"localhost",
     dialect:"mysql",
@@ -16,4 +16,61 @@ router.get('/',async (req,res)=>{
     }
     
 });
+router.get("/:id",async (req,res)=>{
+    try{
+        let author = await Author.findByPk(req.params.id);
+        if(author){
+            res.status(200).json(author)
+        }
+        else{
+            res.status(404).json({error:"couldnt find the Author"})
+        }
+    }catch(error){
+        res.status(500).json({error:error.message});
+    }   
+
+});
+router.post("/",async (req,res)=>{
+    try{
+       const created = await Author.create(req.body);
+        res.status(201).json(created);
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+router.put("/:id",async (req,res)=>{
+    try{
+        const updated = await Author.update(req.body,{
+            where:{
+                author_id:req.params.id
+            }
+        })
+        if(updated){
+            const author = await Author.findByPk(req.params.id);
+            res.status(200).json(author);
+        }
+        else{
+            res.status(404).json({error:"couldnt find the author"})
+        }
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+router.delete("/:id",async (req,res)=>{
+    try{
+        const deleted = await Author.destroy({
+            where:{
+                author_id:req.params.id
+            }
+        });
+        if(deleted){
+            res.status(204).end();
+        }
+        else{
+            res.status(404).json({error:"couldnt find the author"})
+        }
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+})
 module.exports = router;
