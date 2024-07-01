bdb2d8a106ec7fd122596a102e9fe02dfe2593bc
sreevidya <sreevidya-bathalapa@pluralsight.com>
2024-07-01 19:21:44 +0530

    removed put and delete requests from genere routes

--- bookstore-server/routes/genres.js
+++ bookstore-server/routes/genres.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const router = express.Router();
-const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize("bs",'root','Saibaba123456@',{
-    host:"localhost",
-    dialect:"mysql",
-});
+const {sequelize} = require("../models/sequelize");
 const Genere = require("../models/genere")(sequelize);
 router.get('/',async (req,res)=>{
     try{
@@ -39,41 +35,5 @@ router.post('/',async (req,res)=>{
         res.status(500).json({error:error.message});
     }
 });
-router.put('/:id',async (req,res)=>{
-    try{
-        const updated = await Genere.update(req.body,{
-            where:{
-                genre_id:req.params.id
-            }
-        })
-        if(updated){
-            let genere =await Genere.findByPk(req.params.id);
-            res.status(200).json(genere);
 
-        }
-        else{
-            res.status(404).json({error:"Couldn't find the genere"})
-        }
-    }catch(error){
-        res.status(500).json({error:error.message});
-    }
-    
-});
-router.delete('/:id',async (req,res)=>{
-    try{
-        let deleted = await Genere.destroy({
-            where:{
-                genre_id:req.params.id
-            }
-        })
-        if(deleted){
-            res.status(204).end();
-        }
-        else{
-            res.status(404).json({error:"Couldnt find the Genere"})
-        }
-    }catch(error){
-        res.status(500).json({error:error.message})
-    }
-})
 module.exports = router;
